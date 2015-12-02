b44555d1484e6b25a636da5934334316ed52fe36
sakamosa <sakamosa@users.noreply.github.com>
2015-12-01 22:14:35 -1000

    Update slack_slack.js
    Add comments

--- slack_slack.js
+++ slack_slack.js
@@ -1,24 +1,39 @@
+/**********************************************************************
+ Create a new GitHub issue through a custom Slack slash command
+ Sakamoto 12/1/2015
+***********************************************************************/
+
+//include the necessary modules and files
 var config = require('./config.json');
 var http = require('http');
 var Uri = require('jsuri');
 var request = require('request');
 
+//assign the port you will be listening in on
 const port = 3000;
 
+//get the server up and running
 http.createServer(function (req, res) {
 req.setEncoding('utf8');
+
+//sit and wait until some data comes by
 req.on('data', function(data){
 	var uri = new Uri();
+	//parse the url encoded data
 	uri.setQuery(data);
+	//grab the parts we need
 	var input = {token: uri.getQueryParamValue('token'),
                      text: uri.getQueryParamValue('text')
                           };
+	//check that the data was sent from our channel
     	if(input.token !== config.slack_token) {
     		res.writeHead(500, {'Content-Type': 'text/plain'});
   		res.end('incorrect token');
 	}
-	var reply = (input.text + ": ").split(":");
 	var gh_response = "We didn't do anything!";
+	//parse the data text to extract the title and body of our github issue
+	var reply = (input.text + ": ").split(":");
+	//send our POST to GitHub 
 	request({
 	"url":"https://api.github.com/repos/" + config.gh_user + "/" + config.gh_repo + "/issues",
 	"method":"POST",
@@ -37,8 +52,8 @@ req.on('data', function(data){
 		else{
 			gh_response = "New issue successfully created";
 		}
-	
   res.writeHead(200, {"Content-Type": "application/json"});
+  //relay the proper respoinse back to Slack, in-channel means everyone can see it
   var reply = {response_type : "in_channel", text : gh_response};
   var json = JSON.stringify(reply);
   res.end(json);
